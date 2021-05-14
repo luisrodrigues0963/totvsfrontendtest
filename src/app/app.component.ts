a4cc916b972a50abb8b44a97096040224106ec2f
Luís Rodrigues <luis.rodrigues0963@gmail.com>
2021-05-14 11:46:50 +0100

    Upload Projecto

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,40 @@
-import { Component } from '@angular/core';
+
+import { Component, ElementRef, OnInit, Renderer2, ViewChild } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+
+import { PoMenuItem } from '@po-ui/ng-components';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'totvsfrontendtest';
+export class AppComponent implements OnInit{
+
+  label:string = "Documentos de faturação, orçamentos e encomendas";
+  
+
+  saftForm!: FormGroup;
+
+  constructor(private formBuilder: FormBuilder){}
+
+  ngOnInit(): void {
+    this.saftForm = this.formBuilder.group({
+      datainicial:['',Validators.required],
+      datafinal:['',Validators.required],
+      email:['',[Validators.email,Validators.required]]
+    })
+  }
+
+
+
+  readonly menus: Array<PoMenuItem> = [
+    { label: 'Home', action: this.onClick.bind(this) }
+  ];
+
+
+  private onClick() {
+    alert('Clicked in menu item')
+  }
+
 }
